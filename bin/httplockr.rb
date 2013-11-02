63378d147b38b138255890f4c47403fa35827251
Marc Doerflinger <marc@doerf.li>
2013-11-03 00:01:45 +0100

    clear clipboard after 20 sec

--- bin/httplockr.rb
+++ bin/httplockr.rb
@@ -1,11 +1,14 @@
 require 'lockr/http/httplockrinit'
 require 'erb'
 require 'clipboard'  
+require 'rufus/scheduler'
 
 include ERB::Util
 
 init = HttpLockrInit.new()
 init.start()
+
+scheduler = Rufus::Scheduler.new
 
 # now start sinatra
 require 'sinatra'
@@ -26,5 +29,12 @@ get '/copypwd' do
   id = params[:id]
   username = params[:username]
   pwd = Clipboard.copy dir[id][username].password
+  puts 'Password copied to clipboard'
+  
+  scheduler.in '20s' do
+    x = Clipboard.copy ' '
+    puts 'Clipboard cleared'
+  end
+
   redirect '/'
 end
